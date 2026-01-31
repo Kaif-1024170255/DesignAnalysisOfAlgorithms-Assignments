0ded5910e02374e3da11f1f0909801332ca7fda5
Kaif-1024170255 <valorantkaif01@gmail.com>
2026-01-31 23:10:27 +0530

    4th

---
+++ Assignment-1-DAC/Q4MaxSubarraySum.java
@@ -0,0 +1,25 @@
+public class Q4MaxSubarraySum {
+    // O(n^2)
+    public static int getMaxSubarraySum1(int arr[]){
+        int maxSum=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            int sum=arr[i];
+            for(int j=i+1;j<arr.length;j++){
+                sum=sum+arr[j];
+                if(sum>maxSum) maxSum=sum;
+            }
+        }   
+        return maxSum; 
+    }
+    // O(n)
+    public static int getMaxSubarraySum2(int arr[]){
+        int maxSum=Integer.MIN_VALUE;
+        
+        return maxSum;
+    }
+    public static void main(String[] args) {
+        int arr[]={-2, -5, 6, -2, -3, 1, 5, -6};
+        System.out.println(getMaxSubarraySum1(arr));
+        System.out.println(getMaxSubarraySum2(arr));
+    }   
+}
