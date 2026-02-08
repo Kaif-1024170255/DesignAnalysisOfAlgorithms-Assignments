6e7fddee19758e9fc615ee6b44691cb636708ad5
Kaif-1024170255 <valorantkaif01@gmail.com>
2026-02-08 20:30:16 +0530

    A2

--- Assignment-1-DAC/Q4MaxSubarraySum.java
+++ Assignment-1-DAC/Q4MaxSubarraySum.java
@@ -21,6 +21,7 @@ public class Q4MaxSubarraySum {
         }
         return maxSum;
     }
+    // O(nlogn)
     public static void main(String[] args) {
         int arr[]={-2, -5, 6, -2, -3, 1, 5, -6};
         System.out.println(getMaxSubarraySum1(arr));
